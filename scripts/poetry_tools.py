3b42bbe3533c8b4c47b5aab38beb040d9da039a2
Meni Yakove <myakove@gmail.com>
2024-03-25 09:04:13 +0200

    Add get poetry deps script

--- scripts/poetry_tools.py
+++ scripts/poetry_tools.py
@@ -6,7 +6,8 @@ import subprocess
 import shlex
 
 
-def main():
+def get_all_deps():
+    all_deps = []
     if os.path.isfile("pyproject.toml"):
         with open("pyproject.toml", "rb") as fd:
             pyproject = tomllib.load(fd)
@@ -21,19 +22,26 @@ def main():
             if dep == "python":
                 continue
 
-            print(f"Updating {dep}")
-            subprocess.run(shlex.split(f"poetry update {dep}"))
-            for group, deps in group_deps.items():
+            all_deps.append(dep)
+
+            for _, deps in group_deps.items():
                 for dep in deps:
                     if dep == "python":
                         continue
 
-                print(f"Updating group {group} {dep}")
-                subprocess.run(shlex.split(f"poetry update {dep}"))
+                    all_deps.append(dep)
 
     else:
         print("Skipping because it does not have a pyproject.toml")
 
+    return all_deps
+
+
+def update_all_deps():
+    for dep in get_all_deps():
+        print(f"Updating {dep}")
+        subprocess.run(shlex.split(f"poetry update {dep}"))
+
 
 if __name__ == "__main__":
-    main()
+    update_all_deps()
